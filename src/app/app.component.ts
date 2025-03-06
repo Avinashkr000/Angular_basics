69cdca37f1d9076a4434dd3b687e7e61b42714c1
Avinashkr000 <akum97543@gmail.com>
2025-03-06 12:13:51 +0530

    [Avinash kumar]Add. To display Hello from BridgeLabz with a user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
-
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Hello-world-app';
-  message: string = 'Hello from BridgeLabz using One Way Data Bindings';
+  message: string = 'The BridgeLabz logo : a Bridge to Employment through lab works';
   logoUrl: string = 'BL_logo_square_png.jpg';
   openBridgeLabzSite(): void {
     window.open('https://www.bridgelabz.com', '_blank');
   }
+  userName: string = '';
 }
